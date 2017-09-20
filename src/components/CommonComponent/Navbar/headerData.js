34edde125609550045bf7933a8fbce2eefd7f8ab
sourabh-garg <sourabhgrg713@gmail.com>
2017-09-20 20:11:28 +0530

    product page components added

---
+++ src/components/CommonComponent/Navbar/headerData.js
@@ -0,0 +1,143 @@
+export const NavData = {
+
+
+  women: [
+    {
+      type: 'categories',
+      properties: [
+        {
+          displayName: 'Tops',
+          webLink: '/collection/5307279536685056/tops',
+          properties: [
+            {
+              displayName: 'Crop',
+              webLink: '/collection/4655802736574464/crop-top'
+            },
+            {
+              displayName: 'Off Shoulder',
+              webLink: '/collection/4546990109229056/off-shoulder-top'
+            },
+            {
+              displayName: 'Anti-fit',
+              webLink: '/collection/6416131556507648/anti-fit-tops'
+            },
+            {
+              displayName: 'Tunics',
+              webLink: '/collection/5057725910351872/tunics'
+            }
+          ]
+        },
+        {
+          displayName: 'Dresses',
+          webLink: '/collection/5901693883645952/dresses',
+          properties: [
+            {
+              displayName: 'Maxi',
+              webLink: '/collection/5746569643163648/maxi-dress'
+            },
+            {
+              displayName: 'Midi',
+              webLink: '/collection/6544077961166848/midi'
+            },
+            {
+              displayName: 'Shift',
+              webLink: '/collection/5525686722887680/shift-dresses'
+            },
+            {
+              displayName: 'Mini',
+              webLink: '/collection/6730655400591360/mini-dresess'
+            }
+          ]
+        },
+        {
+          displayName: 'Skirts',
+          webLink: '/collection/5326193633132544/skirts',
+          properties: [
+            {
+              displayName: 'Maxi',
+              webLink: '/collection/5801014695428096/maxi-skirts'
+            },
+            {
+              displayName: 'Midi',
+              webLink: '/collection/4632479587762176/midi-skirts'
+            },
+            {
+              displayName: 'Pencil',
+              webLink: '/collection/5318847439699968/pencil-skirts'
+            },
+            {
+              displayName: 'Skater',
+              webLink: '/collection/5530927690285056/skater-skirt'
+            },
+
+          ]
+        },
+        {
+          displayName: 'Bottomwear',
+          webLink: '/collection/4753095879950336/bottomwear',
+          properties: [
+            {
+              displayName: 'Pants',
+              webLink: '/collection/6396525869662208/palazzo-pants'
+            },
+            {
+              displayName: 'Culottes',
+              webLink: '/collection/5643815939604480/culottes'
+            },
+            {
+              displayName: 'Skinny',
+              webLink: '/collection/4906722510635008/skinny-pants'
+            },
+            {
+              displayName: 'Shorts',
+              webLink: '/collection/4999767172186112/shorts'
+            }
+          ]
+        },
+        {
+          displayName: 'Jewellery',
+          webLink: '/collection/5702394918862848/jewellery',
+          properties: [
+            {
+              displayName: 'Rings',
+              webLink: '/collection/5362297577013248/rings'
+            },
+            {
+              displayName: 'Earrings',
+              webLink: '/collection/5411514848772096/earrings'
+            },
+            {
+              displayName: 'Bracelet',
+              webLink: '/collection/5383607862558720/bracelet'
+            }
+          ]
+        },
+        {
+          displayName: 'Shoes',
+          webLink: '/collection/4558215362772992/shoes',
+          properties : []
+        },
+        {
+          displayName: 'Bags',
+          webLink: '/collection/5944427734499328/bags',
+          properties : []
+        },
+        {
+          displayName: 'Sleepwear',
+          webLink: '/collection/5628380590505984/sleepwear',
+          properties : []
+        },
+        {
+          displayName: 'Outerwear',
+          webLink: '/collection/4754125854212096/outerwear',
+          properties : []
+        },
+        {
+          displayName: 'Activewear',
+          webLink: '/collection/6620554274668544/activewear',
+          properties : []
+        }
+      ]
+    }
+  ],
+};
