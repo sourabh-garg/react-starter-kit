8cc9adc0fbe68092932b27bb6cf2fd1422fcd56b
Syed Moahmmed Fazal <fazal@wooplr.com>
2017-09-21 01:44:12 +0530

    Resolving dependencies in footer

--- src/components/CommonComponent/Footer/footer.js
+++ src/components/CommonComponent/Footer/footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import './footer.scss'
+import './footer.scss';
 import FbSvg from '../../../images/Icons/facebook';
 import InstaSvg from '../../../images/Icons/instagram';
 import TwitterSvg from '../../../images/Icons/twitter';
@@ -21,101 +21,52 @@ class Main extends React.Component {
   render() {
 
     return (
-
       <div className="container">
-
         <div className="jumbotron">
-
           <div className="flex footer-div">
-
-            <div>
-
-              <ul className="footer-links">
-
-                <p>COMPANY</p>
-
-                <li>
-                  <a href="">We are Hiring</a>
-                </li>
-                <li>
-                  <a href="">About</a>
-                </li>
-                <li>
-                  <a href="">Collections</a>
-                </li>
-                <li>
-                  <a href="">Track your order</a>
-                </li>
-
+            <div className="footer-links">
+              <p>COMPANY</p>
+              <ul >
+                <li><a href="">We are Hiring</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Collections</a></li>
+                <li><a href="">Track your order</a></li>
               </ul>
-
-
+            </div>
+            <div className="footer-links">
+              <p>LEGAL</p>
+              <ul>
+                <li><a href="">Terms & Conditions</a></li>
+                <li><a href="">Shipping, Exchange & Returns</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Privacy Policy</a></li>
+              </ul>
             </div>
 
-            <div>
-
-              <ul className="footer-links">
-
-                <p>LEGAL</p>
-
-                <li>
-                  <a href="">Terms & Conditions</a>
-                </li>
-                <li>
-                  <a href="">Shipping, Exchange & Returns</a>
-                </li>
-                <li>
-                  <a href="">Contact</a>
-                </li>
-                <li>
-                  <a href="">Privacy Policy</a>
-                </li>
+            <div className="footer-links">
+              <p>DOWNLOAD APP</p>
+              <ul >
 
               </ul>
-
-
-            </div>
-
-            <div>
-
-              <ul className="footer-links">
-                <p>DOWNLOAD APP</p>
-              </ul>
-
               <div className="download-app-div">
                 <a href=""><img src="../../../images/google-play.png" alt=""/></a>
                 <a href=""><img src="../../../images/app-store.png" alt=""/></a>
-
               </div>
-
-
-              <ul className="footer-links">
+              <div className="footer-links">
                 <p>FOLLOW US</p>
-              </ul>
-
+              </div>
               <div className="social-icons">
-
                 <a href=""> <FbSvg /></a>
                 <a href=""> <InstaSvg /></a>
                 <a href=""> <TwitterSvg /></a>
              </div>
-
-
             </div>
-
           </div>
-
         </div>
-
         <hr className="line-break"/>
-
         <div className="jumbotron">
-
           <p className="copyright">© 2017 www.wooplr.com. All rights reserved.</p>
-
-
         </div>
-
       </div>
     );
   }
